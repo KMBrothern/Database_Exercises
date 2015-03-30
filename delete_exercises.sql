37d90fb38362c0bb7eee6b7b4ae3f4e64bc876d9
Keyasha Brothern <kmbrothern85@gmail.com>
2015-03-30 14:25:09 -0500

    Converted select statements to delete statments, output shows query data has been deleted

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,10 +1,10 @@
 USE codeup_test_db;
 
-SELECT 'All albums released after 1991' AS 'All Albums';
-SELECT * FROM albums WHERE release_date > 1991;
+SELECT 'All albums released after 1991 will be deleted' AS 'All Albums';
+DELETE FROM albums WHERE release_date > 1991;
 
-SELECT 'All albums in Disco Genre' AS 'Disco Albums';
-SELECT * FROM albums WHERE genre LIKE '%disco%';
+SELECT 'All albums in Disco Genre will be deleted' AS 'Disco Albums';
+DELETE FROM albums WHERE genre LIKE '%disco%';
 
-SELECT 'All albums by Whitney Houston' AS 'Whitney''s Albums';
-SELECT * FROM albums WHERE artist = 'Whitney Houston';
+SELECT 'All albums by Whitney Houston will be deleted' AS 'Whitney''s Albums';
+DELETE FROM albums WHERE artist = 'Whitney Houston';
