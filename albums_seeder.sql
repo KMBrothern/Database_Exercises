00d324232fb4d9e5c3cc28a44da23d8dc3a617a4
Keyasha Brothern <kmbrothern85@gmail.com>
2015-03-30 10:30:27 -0500

    Practicing inserting to tables mysql, adding and then displaying output in table using SELECT

---
+++ albums_seeder.sql
@@ -0,0 +1,46 @@
+USE codeup_test_db;
+
+INSERT INTO albums (artist, record_name, sales_in_millions, release_date, genre)
+VALUES ('Shania Twain', 'Come On Over', 29.6, 1997, 'Country, pop'),
+        ('Led Zeppelin', 'Led Zepelin IV', 29, 1971, 'Hard rock, heavy metal'),
+        ('Alanis Morissette', 'Jagged Little Pill', 24.8, 1995, 'Alternative rock'),
+        ('Celine Dion', 'Falling into You', 20.2, 1996, 'Pop/Soft rock'),
+        ('The Beatles', 'Sgt. Pepper''s Lonely Hearts Club Band', 13.1, 1967, 'Rock'),
+        ('Eagles', 'Hotel California', 21.5, 1976, 'Rock, soft rock, folk rock'),
+        ('Mariah Carey', 'Music Box', 17.6, 1993, 'Pop / R&B / Rock'),
+        ('Various Artist', 'Dirty Dancing', 17.9, 1987, 'Pop, rock, R&B'),
+        ('Celine Dion', 'Let''s Talk About Love', 19.3, 1997, 'Pop / Soft rock'),
+        ('The Beatles', '1', 21.6, 2000, 'Rock'),
+        ('Adele', '21', 21.3, 2011, 'Pop, soul'),
+        ('The Beatles', 'Abbey Road', 14.4, 1969, 'Rock'),
+        ('Bruce Springsteen', 'Born inthe U.S.A', 19.6, 1984, 'Rock'),
+        ('Dire Straits', 'Brothers in Arms', 18.6, 1985, 'Rock'),
+        ('Whitney Houston', 'Whitney Houston', 17.2, 1985, 'Pop, R&B'),
+        ('James Horner', 'Titanic: Music fro the Motion Picture', 18.1, 1997, 'Soundtrack'),
+        ('Madonna', 'The Immaculate Collection', 19.4, 1990, 'Pop / Dance'),
+        ('Metallica', 'Metallica', 19.9, 1991, 'Thrash metal / heavy metal'),
+        ('Michael Jackson', 'Bad', 18.4, 1987, 'Pop, funk, rock'),
+        ('Michael Jackson', 'Dangerous', 16.3, 1991, 'Rock / Funk / Pop'),
+        ('Nirvana', 'Nevermind', 16.7, 1991, 'Grunge, alternaitive rock'),
+        ('Pink Floyd', 'The Wall', 17.6, 1979, 'Progressive rock'),
+        ('Santana', 'Supernatural', 20.5, 1999, 'Rock'),
+        ('Guns N'' Roses', 'Appetite for Destruction', 21.3, 1987, 'Heavy metal / hard rock'),
+        ('Michael Jackson', 'Thriller', 42.4, 1982, 'Pop, rock, R&B'),
+        ('Pink Floyd', 'The Dark Side of the Moon', 22.7, 1973, 'Progressive rock'),
+        ('Whitney Houston', 'The Bodyguard', 27.4, 1992, 'Soundtrack/R&B, soul, pop'),
+        ('Meat Loaf', 'Bad Out of Hell', 20.6, 1977, 'Hard rock, progressive rock'),
+        ('Eagles', 'Their Greatest Hits (1971-1975)', 32.2, 1976, 'Rock, soft rock, folk rock'),
+        ('AC/DC', 'Back in Black', 25.9, 1980, 'Hard rock'),
+        ('Bee Gees', 'Saturday Night Fever', 19, 1977, 'Disco'),
+        ('Fleetwood Mac', 'Rumours', 27.9, 1977, 'Soft rock');
+
+
+
+
+
+
+
+
+
+
+
