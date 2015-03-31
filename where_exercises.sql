96bc8aaa0715af2914b93f146c90468b0cb10090
Keyasha Brothern <kmbrothern85@gmail.com>
2015-03-31 09:38:43 -0500

    employees hired in the 90's and born on xmas; where queries exercises

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,2 +1,4 @@
 SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 SELECT last_name FROM employees WHERE last_name LIKE 'e%';
+SELECT hire_date FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+SELECT birth_date FROM employees WHERE birth_date LIKE  '%-12-25';
