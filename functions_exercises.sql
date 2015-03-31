ed79cc2e8c5bbff327b5beb82a425e0b01248e19
Keyasha Brothern <kmbrothern85@gmail.com>
2015-03-31 14:35:15 -0500

    used concat on exercise 3 to combine fname and lname into fullname

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,7 +1,9 @@
-SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') AND gender = 'M'
-ORDER BY last_name DESC, first_name DESC;
+SELECT  COUNT(*) 
+FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') 
+GROUP BY gender;
 
-SELECT * FROM employees WHERE last_name LIKE 'e%' AND last_name LIKE '%e'
+SELECT concat(first_name, ' ', last_name) AS full_name
+FROM employees WHERE last_name LIKE 'e%e'
 ORDER BY emp_no DESC;
 
 SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' AND birth_date LIKE '%-12-25'
