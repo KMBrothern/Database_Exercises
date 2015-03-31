a924b850bcffb562747f2cd73f198c53157af911
Keyasha Brothern <kmbrothern85@gmail.com>
2015-03-31 14:06:12 -0500

    update emp born on xmas and hired in the 90's to find only the first 5 employees

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -5,4 +5,8 @@ SELECT DISTINCT last_name FROM employees
 ORDER BY last_name DESC
 LIMIT 10;
 
+SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' AND birth_date LIKE '%-12-25'
+ORDER BY birth_date, hire_date DESC
+LIMIT 5;
+
 /* SELECT DISTINCT last_name FROM employees WHERE last_name LIKE '%q%' AND (last_name NOT LIKE '%qu%'); */
