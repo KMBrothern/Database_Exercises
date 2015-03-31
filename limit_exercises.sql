ff6381c65c03777bc2543ab6d0339531535f66e5
Keyasha Brothern <kmbrothern85@gmail.com>
2015-03-31 14:13:11 -0500

    using emp born on xmas and hired in 90's query update to find the 10 batch of results and list only 5 results

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -7,6 +7,7 @@ LIMIT 10;
 
 SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' AND birth_date LIKE '%-12-25'
 ORDER BY birth_date, hire_date DESC
-LIMIT 5;
+LIMIT 5
+OFFSET 45;
 
 /* SELECT DISTINCT last_name FROM employees WHERE last_name LIKE '%q%' AND (last_name NOT LIKE '%qu%'); */
