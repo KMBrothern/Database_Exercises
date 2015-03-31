cc570264805063e09e06ce937f420e284e35656f
Keyasha Brothern <kmbrothern85@gmail.com>
2015-03-31 15:39:48 -0500

    changed my datediff parameters so that my days w/company wasnt' a negative number

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -6,8 +6,10 @@ SELECT concat(first_name, ' ', last_name) AS full_name
 FROM employees WHERE last_name LIKE 'e%e'
 ORDER BY emp_no DESC;
 
-SELECT first_name, last_name, DATEDIFF(hire_date, now()) AS days_employed_with_company
+SELECT first_name, last_name, DATEDIFF(now(), hire_date) AS days_employed_with_company
 FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' AND birth_date LIKE '%-12-25'
 ORDER BY emp_no;
 
-SELECT * FROM employees WHERE last_name LIKE '%q%' AND (last_name NOT LIKE '%qu%');
+SELECT first_name, last_name 
+FROM employees WHERE last_name LIKE '%q%' AND (last_name NOT LIKE '%qu%')
+ORDER BY first_name, last_name;
