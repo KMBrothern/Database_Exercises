c62bf1041313fbd0966bbbfe17046fc2efca5aff
Keyasha Brothern <kmbrothern85@gmail.com>
2015-03-31 13:47:11 -0500

    limit exercise, list first 10 distinct last name sorted in Desc order

---
+++ limit_exercises.sql
@@ -0,0 +1,8 @@
+/* SELECT DISTINCT title FROM titles
+ORDER BY title; */
+
+SELECT DISTINCT last_name FROM employees
+ORDER BY last_name DESC
+LIMIT 10;
+
+/* SELECT DISTINCT last_name FROM employees WHERE last_name LIKE '%q%' AND (last_name NOT LIKE '%qu%'); */
