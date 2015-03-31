e5d58880c61426961c0b67954448a28c7544d1bd
Keyasha Brothern <kmbrothern85@gmail.com>
2015-03-31 10:18:34 -0500

    find all employees with a 'q' in their name but not 'qu' returns 547 rows

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,3 +1,1 @@
-SELECT hire_date FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' 
-AND (birth_date LIKE  '%-12-25');
-SELECT last_name FROM employees WHERE last_name LIKE '%q%'
+SELECT last_name FROM employees WHERE last_name LIKE '%q%' AND (last_name NOT LIKE '%qu%');
