76546487095f68fbc1be9ee0f0d64f99944acefc
Keyasha Brothern <kmbrothern85@gmail.com>
2015-03-31 11:57:49 -0500

    find the unique combo of fname & lname where lname start&ends w/e. practicing one push

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -1,4 +1,5 @@
 SELECT DISTINCT title FROM titles
 ORDER BY title;
 
-
+SELECT DISTINCT first_name, last_name FROM employees WHERE last_name LIKE 'e%' AND last_name LIKE '%e'
+GROUP BY first_name, last_name;
