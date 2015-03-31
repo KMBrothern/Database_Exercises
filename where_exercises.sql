993095e47957c635516782adaa46fbf817af4926
Keyasha Brothern <kmbrothern85@gmail.com>
2015-03-31 10:00:01 -0500

    find first name w/in 3group and if they are male, returns 441 rows

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,7 +1,6 @@
-SELECT * FROM employees WHERE first_name = 'Irena'
-OR first_name = 'Vidya'
-OR first_name = 'Maya';
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+AND gender = 'M';
 /* SELECT last_name FROM employees WHERE last_name LIKE 'e%';
 SELECT hire_date FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
 SELECT birth_date FROM employees WHERE birth_date LIKE  '%-12-25';
-SELECT last_name FROM employees WHERE last_name LIKE '%q%'; */
+SELECT last_name FROM employees WHERE last_name LIKE '%q%' */
