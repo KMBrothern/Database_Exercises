87c2bc985cd9389f6414bafb21db601a4c72875a
Keyasha Brothern <kmbrothern85@gmail.com>
2015-03-31 09:50:49 -0500

    upadte my IN query to an OR query, returns 709 rows

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,5 +1,7 @@
-SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
-SELECT last_name FROM employees WHERE last_name LIKE 'e%';
+SELECT * FROM employees WHERE first_name = 'Irena'
+OR first_name = 'Vidya'
+OR first_name = 'Maya';
+/* SELECT last_name FROM employees WHERE last_name LIKE 'e%';
 SELECT hire_date FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
 SELECT birth_date FROM employees WHERE birth_date LIKE  '%-12-25';
-SELECT last_name FROM employees WHERE last_name LIKE '%q%';
+SELECT last_name FROM employees WHERE last_name LIKE '%q%'; */
