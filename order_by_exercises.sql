1468b5e1f1c58c045f612b9d224ecff83182c39c
Keyasha Brothern <kmbrothern85@gmail.com>
2015-03-31 11:05:57 -0500

    modified query to order by first name in ASC order

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,5 +1,6 @@
-SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
-SELECT last_name FROM employees WHERE last_name LIKE 'e%';
-SELECT hire_date FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
-SELECT birth_date FROM employees WHERE birth_date LIKE  '%-12-25';
-SELECT last_name FROM employees WHERE last_name LIKE '%q%';
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') AND gender = 'M'
+ORDER BY first_name;
+/* SELECT last_name FROM employees WHERE last_name LIKE 'e%' AND last_name LIKE '%e';
+SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' AND birth_date LIKE '%-12-25';
+-- SELECT birth_date FROM employees WHERE birth_date LIKE  '%-12-25';
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND (last_name NOT LIKE '%qu%'); */
