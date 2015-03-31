9e11be9c7ad0a6d262797e01272f136c3ec96997
Keyasha Brothern <kmbrothern85@gmail.com>
2015-03-31 11:59:20 -0500

    find unique lnames w/'q' but not 'qu' used distinct. Practicing, one push w/several commits

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -3,3 +3,5 @@ ORDER BY title;
 
 SELECT DISTINCT first_name, last_name FROM employees WHERE last_name LIKE 'e%' AND last_name LIKE '%e'
 GROUP BY first_name, last_name;
+
+SELECT DISTINCT last_name FROM employees WHERE last_name LIKE '%q%' AND (last_name NOT LIKE '%qu%');
