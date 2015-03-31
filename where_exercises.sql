42f22a23ed4b193e9c4ed7797ba6e69916f67929
Keyasha Brothern <kmbrothern85@gmail.com>
2015-03-31 10:11:32 -0500

    find all employees whose last name starts and ends w/ e returns 899 rows

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,4 +1,4 @@
-SELECT last_name FROM employees WHERE last_name LIKE 'e%' OR last_name LIKE '%e';
+SELECT last_name FROM employees WHERE last_name LIKE 'e%' AND last_name LIKE '%e';
 SELECT hire_date FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
 SELECT birth_date FROM employees WHERE birth_date LIKE  '%-12-25';
 SELECT last_name FROM employees WHERE last_name LIKE '%q%'
