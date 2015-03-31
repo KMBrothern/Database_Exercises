0f62ecdf07f6e98ca556f2695dd0c8bf7852ec5b
Keyasha Brothern <kmbrothern85@gmail.com>
2015-03-31 15:10:04 -0500

    query of emp born on xmas and hired in the 90's, showing how many days they have been with the company

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,4 +1,4 @@
-SELECT  COUNT(*) 
+SELECT  COUNT(*), gender
 FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') 
 GROUP BY gender;
 
@@ -6,7 +6,8 @@ SELECT concat(first_name, ' ', last_name) AS full_name
 FROM employees WHERE last_name LIKE 'e%e'
 ORDER BY emp_no DESC;
 
-SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' AND birth_date LIKE '%-12-25'
-ORDER BY birth_date, hire_date DESC;
+SELECT first_name, last_name, DATEDIFF(hire_date, now()) AS days_employed_with_company
+FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' AND birth_date LIKE '%-12-25'
+ORDER BY emp_no;
 
 SELECT * FROM employees WHERE last_name LIKE '%q%' AND (last_name NOT LIKE '%qu%');
