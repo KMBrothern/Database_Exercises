1f1cf6ec51a909ea5f4cf4c778932c8b9d824c8e
Keyasha Brothern <kmbrothern85@gmail.com>
2015-03-31 11:14:10 -0500

    reversed sort order for emp w/ e in last name and emp w/first name irena, vidya, or maya who is male. Set to Descinding sort order

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,7 +1,7 @@
-/* SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') AND gender = 'M'
-ORDER BY last_name, first_name; */
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') AND gender = 'M'
+ORDER BY last_name DESC, first_name DESC;
 SELECT * FROM employees WHERE last_name LIKE 'e%' AND last_name LIKE '%e'
-ORDER BY emp_no;
+ORDER BY emp_no DESC;
 SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' AND birth_date LIKE '%-12-25';
 -- SELECT birth_date FROM employees WHERE birth_date LIKE  '%-12-25';
 SELECT * FROM employees WHERE last_name LIKE '%q%' AND (last_name NOT LIKE '%qu%');
