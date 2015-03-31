857d4bbeaafb5c177aae57bf0aaaec7432f49ccf
Keyasha Brothern <kmbrothern85@gmail.com>
2015-03-31 11:55:49 -0500

    using distinct, practicing one push

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -1,7 +1,4 @@
 SELECT DISTINCT title FROM titles
 ORDER BY title;
 
-SELECT DISTINCT first_name, last_name FROM employees WHERE last_name LIKE 'e%' AND last_name LIKE '%e'
-GROUP BY first_name, last_name;
 
-SELECT DISTINCT last_name FROM employees WHERE last_name LIKE '%q%' AND (last_name NOT LIKE '%qu%');
